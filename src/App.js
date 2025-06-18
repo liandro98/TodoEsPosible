43cd5aa93de7c8baf2ecf8ca4eebd1df2b364c6c
liandro98 <miguelalejandroespinosa7@gmail.com>
2025-06-18 14:11:00 -0600

    Correcion de menu

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Content from './components/Content';
-import Menu from './components/Menu';
 
 function App() {
   return (
     <>
       <Header />
-      <Menu />
       <Content />
       <Footer />
     </>
